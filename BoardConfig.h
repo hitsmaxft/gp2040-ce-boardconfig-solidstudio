923ef40442476a974e02b68b4d9a0774eea3fc20
hitsmaxft <mfthits@gmail.com>
2024-05-29 22:22:09 +0800

    Update BoardConfig.h

--- BoardConfig.h
+++ BoardConfig.h
@@ -7,6 +7,7 @@
 #define PICO_BOARD_CONFIG_H_
 
 #include "enums.pb.h"
+#include "class/hid/hid.h"
 
 #define BOARD_CONFIG_LABEL "PicoSolid"
 
@@ -34,11 +35,15 @@
 
 
 // i2c display
-#define HAS_I2C_DISPLAY 1
-#define I2C_BLOCK i2c1
-#define I2C_SDA_PIN 26
-#define I2C_SCL_PIN 27
-#define I2C_SPEED 400000
+#define HAS_I2C_DISPLAY 1 
+#define I2C0_ENABLED 0
+#define I2C1_ENABLED 1
+#define I2C1_PIN_SDA 26
+#define I2C1_PIN_SCL 27
+#define I2C1_SPEED 400000
+
+#define BUTTON_LAYOUT BUTTON_LAYOUT_STICKLESS
+#define BUTTON_LAYOUT_RIGHT BUTTON_LAYOUT_STICKLESSB
 
 #define SPLASH_MODE SPLASH_MODE_STATIC
 #define SPLASH_CHOICE SPLASH_CHOICE_MAIN
